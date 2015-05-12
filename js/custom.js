1fa95718a5df6bade2cf07cd4e9f4f31254e0ab0
Jstaud <james.staud@mavs.uta.edu>
2015-05-12 16:33:20 -0500

    graphics added

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,33 @@
-/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
- * Author:HtmlCoder
- * Author URI:http://www.htmlcoder.me
- * Version:1.0.0
- * Created:November 2014
- * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
- * File Description: Place here your custom scripts
- */
+window.onload = function()
+{
+    $.ajax({
+  url: 'http://api.randomuser.me/?results=6',
+  dataType: 'json',
+  success: function(data){
+    console.log(data);
+    
+    for (var i = 1; i <= 6; i++) {
+    	$("#name"+i).text(capitalizeFirstLetter(data.results[i-1].user.name.first));
+  		$("#image"+i).attr('src', data.results[i-1].user.picture.medium);
+    };
+  }
+});
+
+
+    
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+$(function() {
+    
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
