fa9d58fead13a9bef3539b6595912d37c8cfd098
Cameron Moreau <cameron.cm6@gmail.com>
2015-05-13 21:56:46 -0500

    updated contact form email request

--- php/contact_us.php
+++ php/contact_us.php
@@ -45,7 +45,7 @@ function formParamsPass() {
 }
 
 function mailSent() {
-	$to = 'cameron.cm6@gmail.com';
+	$to = 'james.staud@gmail.com';
 
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
